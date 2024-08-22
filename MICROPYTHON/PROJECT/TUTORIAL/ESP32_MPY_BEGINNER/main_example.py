648532026344df5a0cde0462669d705d47b447c2
Shuaiwen-Cui <SHUAIWEN001@e.ntu.edu.sg>
2024-08-22 22:12:33 +0800

    auto commit by win-upload.bat

--- MICROPYTHON/PROJECT/TUTORIAL/ESP32_MPY_BEGINNER/main_example.py
+++ MICROPYTHON/PROJECT/TUTORIAL/ESP32_MPY_BEGINNER/main_example.py
@@ -1,5 +1,17 @@
+from machine import Pin
+import time
+
+def blink(LED, time_interval=1):
+    LED.value(1)
+    time.sleep(time_interval)
+    LED.value(0)
+    time.sleep(time_interval)
+
 def main():
     print("Welcome to MicroPython!")
+    LED = Pin(1, Pin.OUT)
+    while True:
+        blink(LED)
     
 if __name__ == '__main__':
     main()
